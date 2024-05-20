914bfefa5fec31fbe732bec8e9b6ced9d6c34dae
styzbot <153439890+styzbot@users.noreply.github.com>
2024-05-20 23:47:36 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
+ document.addEventListener("DOMContentLoaded", function() {
+document.body.classList.add("blur");
+document.getElementById("loading-text").classList.add("visible");
+setTimeout(function() {
+document.body.classList.remove("blur");
+document.getElementById("loading-text").classList.remove("visible");
+}, 1500);
+});
+;
+
+
 document.addEventListener("DOMContentLoaded", function() {
     var button = document.getElementById("addMeButton");
 
-    // Add hover effect
     button.addEventListener("mouseover", function() {
         button.querySelector(".group-2").style.backgroundColor = "#004bb5";
     });
@@ -19,18 +29,6 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.body.classList.add("blur");
-    document.getElementById("loading-text").classList.add("visible");
-
-    setTimeout(function() {
-        document.body.classList.remove("blur");
-        document.getElementById("loading-text").classList.remove("visible");
-    }, 1200);
-});
-;
-
-
 $('a[href*="#"]').on('click', function (e) {
     e.preventDefault();
 
@@ -38,48 +36,6 @@ $('a[href*="#"]').on('click', function (e) {
         scrollTop: $($(this).attr('href')).offset().top
     }, 500, 'linear');
 });
-
-$(window).on('mousewheel', function (e) {
-    e.preventDefault();
-    var delta = e.originalEvent.wheelDelta;
-    var scrollDistance = 100;
-
-    $('html, body').stop().animate({
-        scrollTop: $(window).scrollTop() - (scrollDistance * delta / Math.abs(delta))
-    }, 150, 'linear');
-});
-
-
-$(document).ready(function () {
-    $(window).scroll(function () {
-        var scrollTop = $(window).scrollTop();
-        var windowHeight = $(window).height();
-        
-        $('.stage').each(function () {
-            var offset = $(this).offset().top;
-            var height = $(this).outerHeight();
-
-            if (scrollTop > offset - windowHeight && scrollTop < offset + height) {
-                $(this).find('.line').css('opacity', 1);
-            } else {
-                $(this).find('.line').css('opacity', 0);
-            }
-        });
-    });
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-    const scrollableElement = document.documentElement;
-
-    scrollableElement.addEventListener("scroll", function() {
-        const isScrollBarClicked = window.scrollY !== scrollableElement.scrollTop;
-        if (isScrollBarClicked) {
-            const targetElement = document.getElementById("element");
-            targetElement.scrollIntoView({ behavior: "smooth" });
-        }
-    });
-});
-
 
 function setDynamicBackground() {
     const screenWidth = window.innerWidth;
@@ -98,5 +54,4 @@ function setDynamicBackground() {
 
 window.addEventListener('DOMContentLoaded', setDynamicBackground);
 window.addEventListener('resize', setDynamicBackground);
-
 
